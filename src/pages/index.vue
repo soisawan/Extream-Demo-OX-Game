2bee97d82d993a7bc15490962edd0c3fb9677f4f
Soisawan Imwibul <soisawan.im@inet.co.th>
2024-10-09 23:45:39 +0700

    initial project

---
+++ src/pages/index.vue
@@ -0,0 +1,192 @@
+<template>
+  <v-app class="animated-background">
+    <v-main>
+      <v-container class="d-flex flex-column align-center justify-center fill-height text-center">
+        <v-card class="mx-auto elevation-3" outlined style="border-radius: 30px; padding: 30px;" width="500">
+          <v-card-title class="text-h5" style="font-size: 30px; font-weight: bold;"><h2>Welcome To Tic Tac Toe</h2></v-card-title>
+          <v-card-text>
+            <v-avatar v-if="user" class="mx-auto mb-3" size="120">
+              <v-img alt="User Avatar" :src="user?.picture" />
+            </v-avatar>
+            <h3 v-if="user" class="mb-2" style="font-size: 24px; font-weight: bold;">{{ user?.name }}</h3>
+            <v-card
+              v-if="user"
+              class="mx-auto mb-4 elevation-2"
+              max-width="300px"
+              outlined
+              style="border-radius: 20px; background: linear-gradient(10deg, #3B76EF, #ac9cff ); color: white; transition: all 0.3s ease;"
+              @mouseleave="hover = false"
+              @mouseover="hover = true"
+            >
+              <v-card-title class="text-h5 mb-n2" style="font-weight: bold; color: white;">
+                Total Score
+              </v-card-title>
+              <v-card-subtitle
+                class="font-weight-bold"
+                style="font-size: 26px; font-weight: bold; color:white ; text-shadow: 2px 20px 10px rgba(0, 0, 0, 0.5);"
+              >
+                {{ score }}
+              </v-card-subtitle>
+            </v-card>
+            <div v-else class="welcome-card">
+              <img
+                alt="Welcome to Tic Tac Toe"
+                class="welcome-image"
+                src="../assets/img/logo.png"
+              >
+            </div>
+
+            <v-btn
+              v-if="!user"
+              class="rounded-pill rainbow rainbow-2"
+              elevation="2"
+              style="font-size: 20px; width: 200px; height: 50px;"
+              @click="login"
+            >
+              Login
+            </v-btn>
+
+            <v-btn
+              v-else
+              class="rounded-pill rainbow rainbow-1"
+              elevation="3"
+              style="font-size: 20px; width: 200px; height: 50px;"
+              to="/ox-game"
+            >
+              Start
+            </v-btn>
+
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import { useUserStore } from '@/stores/user'
+  import { useAuth0 } from '@auth0/auth0-vue'
+  import axios from 'axios'
+
+  const auth0 = useAuth0()
+  const userStore = useUserStore()
+  const score = ref('กำลังโหลด...')
+  const hover = ref(false)
+
+  const fetchProfile = async (userId: string) => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_APP_API_URL}/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${import.meta.env.VITE_APP_API_TOKEN}`,
+        },
+      })
+      return response.data
+    } catch (error) {
+      console.error('Error fetching profile:', error)
+    }
+  }
+
+  const login = async () => {
+    await auth0.loginWithRedirect()
+  }
+
+  const user = computed(() => {
+    return auth0.user.value
+  })
+
+  watch(user, async (data, oldVal) => {
+    if (data) {
+      const userId = data.sub
+      const profile = await fetchProfile(userId!)
+      if (profile.user_metadata) {
+        score.value = profile.user_metadata.score
+        userStore.score = profile.user_metadata.score
+      } else {
+        score.value = '0'
+      }
+    }
+  })
+</script>
+
+<style scoped>
+html, body {
+  margin:0;
+  padding:0;
+  height:100%;
+}
+
+.rainbow{
+  background-color: #76b8fa;
+  border-radius: 4px;
+  color: #fff;
+  cursor: pointer;
+  padding: 8px 16px;
+
+}
+
+.rainbow-1:hover{
+   background-image: linear-gradient(90deg, #70dafd 0%, #fae482 49%, #ff8989 80%, #70dafd 100%);
+   animation:slidebg 5s linear infinite;
+}
+
+.rainbow-2:hover{
+   background-image: linear-gradient(90deg, #00C0FF 0%, #ff9844 49%, #ffe986 80%, #00C0FF 100%);
+   animation:slidebg 5s linear infinite;
+}
+
+@keyframes slidebg {
+  to {
+    background-position:20vw;
+  }
+}
+
+.animated-background {
+  background: linear-gradient(200deg,#fdeb9a , #ffcccc, #3b65b3);
+  animation: bgColorFade 15s ease infinite;
+}
+
+@keyframes bgColorFade {
+  0% { background-color: #ffcccc; }
+  25% { background-color: #ffd9cc; }
+  50% { background-color: #a3e1fe; }
+  75% { background-color: #ffe6cc; }
+  100% { background-color: #ffcccc; }
+}
+
+.welcome-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 300px;
+  border-radius: 20px;
+}
+
+.welcome-image {
+  max-width: 60%;
+  border-radius: 50%;
+  border: white solid 8px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  animation: spin 10s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.welcome-text {
+  font-size: 28px;
+  font-weight: bold;
+  color: #333;
+}
+
+.v-card:hover {
+  transform: scale(1.03);
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
+}
+</style>
---
+++ src/pages/ox-game.vue
@@ -0,0 +1,436 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      class="custom-app-bar"
+      color="transparent"
+      dark
+      flat
+      height="1px"
+    >
+      <v-spacer />
+      <div class="half-circle">
+        <div class="score-container">
+          <span class="score player">{{ playerScore }}</span>
+          <span class="divider">:</span>
+          <span class="score bot">{{ botScore }}</span>
+        </div>
+      </div>
+      <v-spacer />
+      <div class="profile-container d-flex align-center">
+        <div class="profile-content">
+          <v-avatar v-if="username" class="mr-2 ml-2" size="35">
+            <v-img alt="User Avatar" :src="pic" />
+          </v-avatar>
+          <span v-if="username">{{ username }}</span>
+        </div>
+
+        <v-menu offset-y>
+          <template #activator="{ props }">
+            <v-btn icon v-bind="props">
+              <v-icon>mdi-chevron-down</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item @click="logout">
+              <v-list-item-title>
+                <v-icon color="red" left>mdi-logout</v-icon> Logout
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </v-app-bar>
+
+    <v-main :style="{ background: colorBg }">
+      <v-row
+        style=" flex-direction: row;
+                             align-items: center"
+      >
+        <v-col cols="6">
+          <v-img
+            alt="avatar"
+            class="center-avatar"
+            height="100"
+            :src="img"
+            width="100"
+          />
+          <div class="turn-container">
+            <h3>{{ status }}</h3>
+          </div>
+          <div class="high-score-container">
+            <h3>Total Score: {{ totalScore }}</h3>
+          </div>
+        </v-col>
+        <v-col cols="6">
+          <div class="tic-tac-toe">
+            <div v-for="(cell, index) in board" :key="index" class="cell" @click="makeMove(index)">
+              {{ cell }}
+            </div>
+          </div>
+          <div class="mt-4">
+            <v-btn class="rounded-pill rainbow rainbow-1" color="white" style="font-size: 20px; width: 200px; height: 50px;" @click="resetGame">
+              NEW GAME
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import { onMounted, ref } from 'vue'
+  import Notiflix from 'notiflix'
+  import { useAuth0 } from '@auth0/auth0-vue'
+  import router from '@/router'
+  import botImage from '../assets/img/bot.png'
+  import playerImage from '../assets/img/player.png'
+  import axios from 'axios'
+  import { useUserStore } from '@/stores/user'
+
+  const auth0 = useAuth0()
+  const username = auth0.user.value?.name
+  const pic = auth0.user.value?.picture
+  const isAuthenticated = auth0.isAuthenticated
+  const userId = auth0.user.value?.sub
+  const userStore = useUserStore()
+  let isFinish = false
+
+  const board = ref(['', '', '', '', '', '', '', '', ''])
+  const playerScore = ref(0)
+  const botScore = ref(0)
+  const consecutiveWins = ref(0)
+  const currentPlayer = ref('X')
+  let totalScore = userStore.score
+
+  const colorBg = ref('linear-gradient(150deg, #6d9dff, #c4b9fd, #5d90f8 )')
+  const status = ref('YOUR TURN')
+  const img = ref(playerImage)
+
+  const updateScore = async (isWin: boolean) => {
+    if (isWin) {
+      consecutiveWins.value += 1
+      playerScore.value += 1
+      totalScore += 1
+      status.value = 'YOU WIN!!'
+      if (consecutiveWins.value === 3) {
+        // Award bonus point after 3 consecutive wins
+        status.value = 'YOU WIN 3 CONSECUTIVE!!'
+        playerScore.value += 1
+        totalScore += 1
+        Notiflix.Notify.success('คุณได้รับคะแนนพิเศษสำหรับการชนะติดต่อกัน 3 ครั้ง!', {
+          position: 'right-bottom',
+        })
+        consecutiveWins.value = 0 // Reset streak
+      }
+    } else {
+      if (totalScore > 0) {
+        totalScore -= 1
+      }
+      if (playerScore.value > 0) {
+        playerScore.value -= 1
+      }
+      botScore.value += 1
+      consecutiveWins.value = 0 // Reset streak on loss
+      status.value = 'YOU LOSE'
+    }
+    await updateUserScoreInAuth0(totalScore)
+    Notiflix.Notify.success(`คะแนนปัจจุบันของคุณ: ${playerScore.value}`, {
+      position: 'right-bottom',
+    })
+  }
+
+  const checkWinner = (board: string[]) => {
+    const winningCombinations = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6],
+    ]
+
+    for (const combo of winningCombinations) {
+      const [a, b, c] = combo
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a]
+      }
+    }
+    return null
+  }
+
+  const minimax = (board: string[], depth: number, isMaximizing: boolean) => {
+    const winner = checkWinner(board)
+    if (winner === 'O') return 10 - depth
+    if (winner === 'X') return depth - 10
+    if (!board.includes('')) return 0
+
+    if (isMaximizing) {
+      let bestScore = -Infinity
+      for (let i = 0; i < board.length; i++) {
+        if (board[i] === '') {
+          board[i] = 'O'
+          const score = minimax(board, depth + 1, false)
+          board[i] = ''
+          bestScore = Math.max(score, bestScore)
+        }
+      }
+      return bestScore
+    } else {
+      let bestScore = Infinity
+      for (let i = 0; i < board.length; i++) {
+        if (board[i] === '') {
+          board[i] = 'X'
+          const score = minimax(board, depth + 1, true)
+          board[i] = ''
+          bestScore = Math.min(score, bestScore)
+        }
+      }
+      return bestScore
+    }
+  }
+
+  const makeBotMove = () => {
+    colorBg.value = 'linear-gradient(150deg, #6d9dff, #c4b9fd, #5d90f8 )'
+    status.value = 'YOUR TURN'
+    img.value = playerImage
+
+    let bestScore = -Infinity
+    let move = -1
+    for (let i = 0; i < board.value.length; i++) {
+      if (board.value[i] === '') {
+        board.value[i] = 'O'
+        const score = minimax(board.value, 0, false)
+        board.value[i] = ''
+        if (score > bestScore) {
+          bestScore = score
+          move = i
+        }
+      }
+    }
+    if (move !== -1) {
+      board.value[move] = 'O'
+      currentPlayer.value = 'X'
+    }
+  }
+
+  const makeMove = (index: number) => {
+    if (isFinish) return
+    if (!board.value[index] && currentPlayer.value === 'X') {
+      board.value[index] = 'X'
+      currentPlayer.value = 'O'
+      colorBg.value = 'linear-gradient(190deg, #fea1d2, #ffcccc, #5d90f8 )'
+      status.value = 'BOT TURN'
+      img.value = botImage
+      if (checkDraw()) {
+        Notiflix.Notify.info('เกมเสมอ!', {
+          position: 'right-bottom',
+        })
+        status.value = 'DRAW'
+        isFinish = true
+      } else {
+        const winner = checkWinner(board.value)
+        if (!winner) {
+          setTimeout(() => {
+            makeBotMove() // Bot makes a move
+            const winnerAfterBot = checkWinner(board.value)
+            if (winnerAfterBot) {
+              isFinish = true
+              if (winnerAfterBot === 'X') {
+                Notiflix.Notify.success('คุณชนะ!', {
+                  position: 'right-bottom',
+                })
+                updateScore(true) // Player wins
+              } else if (winnerAfterBot === 'O') {
+                Notiflix.Notify.failure('คุณแพ้!', {
+                  position: 'right-bottom',
+                })
+                updateScore(false) // Player loses
+              }
+            }
+          }, 1000)
+        } else {
+          isFinish = true
+          Notiflix.Notify.success(`${winner} ชนะ!`, {
+            position: 'right-bottom',
+          })
+          updateScore(winner === 'X') // Determine if player won
+        }
+      }
+    }
+  }
+
+  const updateUserScoreInAuth0 = async (newScore: number) => {
+    try {
+      const tokenResponse = import.meta.env.VITE_APP_API_TOKEN
+      const accessToken = tokenResponse
+
+      await axios.patch(
+        `https://dev-4q1tggqlrj05l3o1.us.auth0.com/api/v2/users/${userId}`,
+        {
+          user_metadata: {
+            score: newScore,
+          },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+
+      Notiflix.Notify.success('คะแนนถูกบันทึกแล้ว', {
+        position: 'right-bottom',
+      })
+    } catch (error) {
+      console.error('Error updating score:', error)
+      Notiflix.Notify.failure('ไม่สามารถบันทึกคะแนนได้', {
+        position: 'right-bottom',
+      })
+    }
+  }
+
+  const checkDraw = () => {
+    return board.value.every(cell => cell !== '') && !checkWinner(board.value)
+  }
+
+  const resetGame = () => {
+    board.value = ['', '', '', '', '', '', '', '', '']
+    currentPlayer.value = 'X'
+    colorBg.value = 'linear-gradient(150deg, #6d9dff, #c4b9fd, #5d90f8 )'
+    img.value = playerImage
+    status.value = 'YOUR TURN'
+    isFinish = false
+    Notiflix.Notify.success('เริ่มเกมใหม่เรียบร้อย', {
+      position: 'right-bottom',
+    })
+  }
+
+  const logout = () => {
+    auth0.logout({
+      logoutParams: {
+        returnTo: window.location.origin,
+      },
+    })
+    Notiflix.Notify.success('ออกจากระบบเรียบร้อยแล้ว', {
+      position: 'right-bottom',
+    })
+    window.location.href = '/'
+  }
+
+  onMounted(() => {
+    if (!isAuthenticated.value) {
+      router.push('/')
+    }
+  })
+</script>
+
+<style scoped>
+.center-avatar {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.custom-app-bar {
+  min-height: 110px;
+}
+.tic-tac-toe {
+  display: grid;
+  grid-template-columns: repeat(3, 150px);
+  grid-template-rows: repeat(3, 150px);
+  gap: 10px;
+  margin-top: 20px;
+}
+
+.cell {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 3rem;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  width: 150px;
+  height: 150px;
+}
+
+/* Flexbox เพื่อให้ตารางอยู่ตรงกลางหน้าจอ */
+.v-main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* height: 100vh; */
+}
+
+.half-circle {
+  width: 200px;
+  height: 100px;
+  background-color: white;
+  border-bottom-left-radius: 90px;
+  border-bottom-right-radius: 90px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.score-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 50%;
+}
+
+.score {
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+.player {
+  color: #50c6fc;
+}
+
+.bot {
+  color: #FF5733;
+}
+
+.divider {
+  font-size: 1.5rem;
+  color: black;
+}
+
+.high-score-container {
+  margin-top: 20px;
+  font-size: 1.5rem;
+  color: #333;
+  text-align: center;
+  flex-direction: column;
+}
+.turn-container {
+  margin-top: 20px;
+  font-size: 2rem;
+  color: #333;
+  text-align: center;
+  flex-direction: column;
+}
+
+.profile-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: auto;
+  background-color: white;
+  border-top-left-radius: 50px;
+  border-bottom-left-radius: 50px;
+  padding: 10px;
+}
+
+.profile-content {
+  display: flex;
+  align-items: center;
+  margin-right: auto;
+}
+
+.white--text {
+  color: white;
+}
+</style>
